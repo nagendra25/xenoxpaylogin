d1494dcbb421faccb7aff78554429c891bc87ffd
Nagendra Yadav <nagendrayadav0786@gmail.com>
2020-04-16 23:16:30 +0530

    comit3231

--- app/src/main/java/com/xenox/loginpage/MainActivity.java
+++ app/src/main/java/com/xenox/loginpage/MainActivity.java
@@ -14,7 +14,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String naw1 =LoginClass.getDistanceInKm();
 
-        Log.d("new232",naw1 );
+        String api242 =LoginClass.userLoginRequest(MainActivity.this,"7710910181","nagesh@123","2414414fsffsdfsfsfsfsa2");
+
+        Log.d("new232",api242 );
 
 
 
