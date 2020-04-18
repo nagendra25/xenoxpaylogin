6d3db5646701069f995f1a272a9d3cb047bb2c28
Nagendra Yadav <nagendrayadav0786@gmail.com>
2020-04-18 13:07:32 +0530

    save3232

---
+++ loginpage/src/main/java/com/xenox/loginpage/XenoxPayLogon.java
@@ -0,0 +1,15 @@
+package com.xenox.loginpage;
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class XenoxPayLogon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xenox_pay_logon);
+    }
+}
